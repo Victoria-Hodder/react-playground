8721d127ae9093151d03c650098150cde3e7f498
Victoria Hodder <vrhodder@posteo.net>
2020-04-07 17:34:36 +0200

    learned how to use props in both functional and class components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import { Greet } from './components/greet';
+import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
 
@@ -10,9 +10,19 @@ class App extends Component {
     render () {
       return (
       <div className="App">
-        {/* <Greet /> */}
-        {/* <Welcome /> */}
-        <Hello />
+        <Greet name="Diana" heroName="Wonder Woman">
+        <p>This is a children props</p>
+        </Greet>
+        <Greet name="Carol" heroName="Captain Marvel">
+          <button>This is a button</button>
+        </Greet>
+        <Greet name="Natasha" heroName="Black Widow"/>
+
+        <Welcome name="Diana" heroName="Wonder Woman"/>
+        <Welcome name="Carol" heroName="Captain Marvel"/>
+        <Welcome name="Natasha" heroName="Black Widow"/>
+
+        {/* <Hello /> */}
       </div>
     );
   }
--- src/components/greet.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-// function Greet() {
-//     return <h1>Hi Victoria</h1>
-// }
-
-export const Greet = () =>  <h1>Hi Victoria</h1>
-
-// export default Greet
