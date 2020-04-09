0fbfedef328a8d262b6ee9ac596ea3ec24d57f97
Victoria Hodder <vrhodder@posteo.net>
2020-04-09 15:05:11 +0200

    tidied app.js and added snippets file to gitignore

--- src/App.js
+++ src/App.js
@@ -4,25 +4,14 @@ import './App.css';
 import Greet from './components/Greet';
 import Welcome from './components/Welcome';
 import Hello from './components/Hello';
+import Message from './components/Message';
 
 
 class App extends Component {
     render () {
       return (
       <div className="App">
-        <Greet name="Diana" heroName="Wonder Woman">
-        <p>This is a children props</p>
-        </Greet>
-        <Greet name="Carol" heroName="Captain Marvel">
-          <button>This is a button</button>
-        </Greet>
-        <Greet name="Natasha" heroName="Black Widow"/>
-
-        <Welcome name="Diana" heroName="Wonder Woman"/>
-        <Welcome name="Carol" heroName="Captain Marvel"/>
-        <Welcome name="Natasha" heroName="Black Widow"/>
-
-        {/* <Hello /> */}
+        <Message />
       </div>
     );
   }
