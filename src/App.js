90ff2dc9ba4066106d76234d13c8932deee1fbdb
Victoria Hodder <vrhodder@posteo.net>
2020-04-15 14:22:33 +0200

    video 16: conditional rendering

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import ParentComponent from './components/ParentComponent';
+import UserGreeting from './components/UserGreeting';
 
 class App extends Component {
     render () {
       return (
       <div className="App">
-        <ParentComponent />
+        <UserGreeting />
       </div>
     );
   }
