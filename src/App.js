153af9c8f2c39e004a71ea82410fc1d059867480
Victoria Hodder <vrhodder@posteo.net>
2020-04-15 14:04:55 +0200

    video 15: learning about parent and child components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import EventBind from './components/EventBind';
+import ParentComponent from './components/ParentComponent';
 
 class App extends Component {
     render () {
       return (
       <div className="App">
-        <EventBind />
+        <ParentComponent />
       </div>
     );
   }
