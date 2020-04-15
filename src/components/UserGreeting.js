ac7472bfac035ed7e2d8a73e43becfe02d83161c
Victoria Hodder <vrhodder@posteo.net>
2020-04-15 14:33:18 +0200

    replaced notes for video 16 to see all conditional rendering alternatives

--- src/components/UserGreeting.js
+++ src/components/UserGreeting.js
@@ -10,9 +10,50 @@ class UserGreeting extends Component {
         }
     }
 
+    // Short circuit operator
     render() {
         return this.state.isLoggedIn && <div>Welcome Victoria</div>
     }
+
+
+    // // Ternary conditional operator
+    // render() {
+
+    //     return (
+    //         this.state.isLoggedIn ? (
+    //             <div> Welcome Victoria </div>) : (
+    //                 <div> Welcome Guest </div>
+    //             )
+    //     )
+    // }
+
+
+
+    // // element variables
+    // render() {
+    //     let message
+    //     if (this.state.isLoggedIn) {
+    //         message = <div> Welcome Victoria </div>
+    //     } else {
+    //         message = <div> Welcome Guest </div>
+
+    //     }
+
+    //     return <div> {message} </div>
+    // }
+
+    // // if else statement
+    // render() {
+    //     if (this.state.isLoggedIn) {
+    //         return <div> Welcome Victoria </div>
+    //     } else {
+    //         return <div> Welcome Guest </div>
+    //     }
+
+
+    // }
+
+
 }
 
 export default UserGreeting 
