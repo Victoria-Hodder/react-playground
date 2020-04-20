b805df70e737ab6eae3a3147229f5c3cc312c390
Victoria Hodder <vrhodder@posteo.net>
2020-04-20 15:40:38 +0200

    video 20: CSS basics incl. inline styles and css modules

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
-import UserGreeting from './components/UserGreeting';
-import NameList from './components/NameList';
+import './appStyles.css';
+import styles from './appStyles.module.css';
 
 class App extends Component {
     render () {
       return (
       <div className="App">
-        <NameList />
+        <h1 className='error'>Error</h1>
+        <h1 className={styles.success}>Success</h1>
       </div>
     );
   }
