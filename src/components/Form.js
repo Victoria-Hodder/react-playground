9e5072f27d0fbc5521bce81463e76340ca51ef3c
Victoria Hodder <vrhodder@posteo.net>
2020-04-21 17:17:35 +0200

    video 21, part b: added to form.js component with text and select features

--- src/components/Form.js
+++ src/components/Form.js
@@ -6,7 +6,9 @@ class Form extends Component {
         super(props)
 
         this.state = {
-            username: ''
+            username: '',
+            comments: '',
+            topic: 'react'
         }
 
     }
@@ -18,13 +20,45 @@ class Form extends Component {
 
     }
 
+    handleCommentsChange = (event) => {
+        this.setState ({
+            comments: event.target.value
+        })
+    }
+
+    handleTopicChange = (event) => {
+        this.setState ({
+            topic: event.target.value
+        })
+    }
+
+    handleSubmit = (event) => {
+        alert(`${this.state.username} ${this.state.comments} ${this.state.topic}`)
+        event.preventDefault()
+    }
+
+
+
     render() {
         return (
-            <form>
+            <form onSubmit={this.handleSubmit}>
                 <div>
                     <label>Username</label>
                     <input type='text' value={this.state.username} onChange={this.handleUsernameChange}/>
                 </div>
+                <div>
+                    <label>Comments</label>
+                    <textarea value={this.state.comments} onChange={this.handleCommentsChange}></textarea>
+                </div>
+                <div>
+                    <label value={this.state.topic} onChange={this.handleTopicChange}>Topic</label>
+                    <select>
+                        <option value="react">React</option>
+                        <option value="angular">Angular</option>
+                        <option value="vue">Vue</option>
+                    </select>
+                </div>
+                <button type='submit'>Submit</button>
             </form>
         )
     }
