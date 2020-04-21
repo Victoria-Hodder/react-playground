85f3224d3ffd1e487ed80d49754a9d24c5f0d10f
Victoria Hodder <vrhodder@posteo.net>
2020-04-21 14:48:46 +0200

    Video 21: new component (form.js) to create a controlled component with a username

--- src/App.js
+++ src/App.js
@@ -1,15 +1,14 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
-import './App.css';
-import './appStyles.css';
-import styles from './appStyles.module.css';
+// import './App.css';
+// import './appStyles.css';
+import Form from './components/Form';
 
 class App extends Component {
     render () {
       return (
       <div className="App">
-        <h1 className='error'>Error</h1>
-        <h1 className={styles.success}>Success</h1>
+        <Form />
       </div>
     );
   }
